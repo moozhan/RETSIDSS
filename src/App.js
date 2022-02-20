dc2f5886222836ab1e9a0debff18eae644395598
Moozhan <moozhansh@gmail.com>
2022-02-20 23:23:07 +0000

    main Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 import { Route, Switch } from 'react-router-dom';
 import Main from './pages/Main';
-import Areas from './pages/Areas';
-import TypeofInfrastructure from './pages/TypeofInfrastructure';
-import Priorities from './pages/Priorities';
-import Test from './pages/Test';
-import Location from './pages/Location';
-import TempLocation from './pages/TempLocation';
-import Configure from './pages/Configure';
 
 
 function App() {
@@ -14,27 +7,6 @@ function App() {
       <Switch>
         <Route path='/' exact>
           <Main />
-        </Route>
-        <Route path='/areas'>
-          <Areas />
-        </Route>
-        <Route path='/TypeofInfrastructure'>
-          <TypeofInfrastructure />
-        </Route>
-        <Route path='/priorities'>
-          <Priorities />
-        </Route>
-        <Route path='/test'>
-          <Test />
-        </Route>
-        <Route path='/location'>
-          <Location />
-        </Route>
-        <Route path='/templocation'>
-          <TempLocation />
-        </Route>
-        <Route path='/configure'>
-          <Configure />
         </Route>
       </Switch>
   );
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,23 +1,340 @@
-import React from 'react';
+import React, {useState, useContext} from 'react';
 import { Link } from 'react-router-dom';
+import { MapContainer, TileLayer, LayersControl, GeoJSON, FeatureGroup, LayerGroup} from 'react-leaflet';
+import { EditControl} from "react-leaflet-draw";
+import 'leaflet/dist/leaflet.css';
+import Natuura2000 from '../components/mapLayers/Natuura2000';
+import { multipleGeojson } from "../data/Natuurasingle";
+import "leaflet-draw/dist/leaflet.draw.css";
+import L, { map } from 'leaflet';
+import { useEffect } from 'react/cjs/react.development';
 import Layout from '../components/layout/Layout';
+import './main.css';
+import {rooftopenergyStateContext, turbinenergyValueStateContext} from '../Store';
+import miscanthus from '../components/images/Miscanthus.png';
+import maze from '../components/images/maze.png';
+import cattail from '../components/images/cattail.png';
+import reed from '../components/images/reed.png';
+import buffer from "@turf/buffer";
+import Plot from 'react-plotly.js';
+import Bargraph from '../components/mapdetails/Graph';
+import StackedBargraph from '../components/mapdetails/StackedGraph';
 
-function Main(){
+function TempLocation() {
+    // const [rooftopenergy, setrooftopenergy] = useContext(rooftopenergyStateContext);
+    // const [turbinenergyValue, setturbinenergyValue] = useContext(turbinenergyValueStateContext);
+    // const [added, setTotalEnergy] = useState(0);
+    // const [seconds, setSeconds] = useState(0);
+    // const [years, setYears] = useState(2021);
+    // const [hidden, setHidden] = useState(false);
+    // const [hiddentoo, setHiddentoo] = useState(false);
+    // const [turbinvalue, setturbinvalue] = useState(3.5);
+    // const totalArea = mapLayers.reduce((totalarea, area) => totalarea + area.area, 0);
+    // const energyfarm = totalArea* 750/10000;
+    // const agriculture = totalArea/10000;
+    // const totalEnergy = energyfarm + added + rooftopenergy;
+    // const lefttogoal =  147000 - totalEnergy;
+    // const farm = energyfarm.toFixed(2);
+    // const smalltotalEnergy = totalEnergy.toFixed(2);
 
+    // useEffect(() => {
+    //     const interval = setInterval(() => {
+    //       setYears(year => year + 1);
+    //       if (added !==0){
+    //       setTotalEnergy(added => added + turbinenergyValue);
+    //       }
+    //       setSeconds(seconds => seconds + 1);
+    //     }, 480000);
+    //     return () => clearInterval(interval);
+    //   }, []);
+    //console.log(turbinenergyValue);
+
+    const [mapLayers, setMapLayers] = useState([]);
+    const [totalMisArea, setTotalMisArea] = useState(0);
+    const [totalMaizArea, setTotalMaizArea] = useState(0);
+    const [totalCatArea, setTotalCatArea] = useState(0);
+    const [totalReedArea, setTotalReedArea] = useState(0);
+    const [totalArea, settotalArea] = useState(0);
+    const [totalMisEnergy, setTotalMisEnergy] = useState(0);
+    const [totalMaizEnergy, setTotalMaizEnergy] = useState(0);
+    const [totalCatEnergy, setTotalCatenergy] = useState(0);
+    const [totalReedenergy, setTotalReedenergy] = useState(0);
+    const [totalEnergy, settotalEnergy] = useState(0);
+    const [totalMisCarbon, setTotalMisCarbon] = useState(0);
+    const [totalMaizCarbon, setTotalMaizCarbon] = useState(0);
+    const [totalCatCarbon, setTotalCatCarbon] = useState(0);
+    const [totalReedCarbon, setTotalReedCarbon] = useState(0);
+    const [totalCarbon, settotalCarbon] = useState(0);
+    const [map, setMap] = useState(0);
+    const [bufferValue, setBufferValue] = useState(0);
+    const [draggable, setDraggable] = useState(false);
+
+    // Buffer =====================//
+    var buffered = buffer(multipleGeojson, 500, {units: 'meters'});
+
+    //======ShapeOptions: setting the color of the polygon ===//
+    var Miscanthus = {            
+        fillColor: 'white',
+        weight: 1,
+        opacity: 1,
+        color: 'black',
+        dashArray: '2',
+        fillOpacity: 0.5
+    }
+    var Maize = {            
+        fillColor: 'red',
+        weight: 1,
+        opacity: 1,
+        color: 'black',
+        dashArray: '2',
+        fillOpacity: 0.5
+    }
+    var Cattail = {            
+        fillColor: 'blue',
+        weight: 1,
+        opacity: 1,
+        color: 'black',
+        dashArray: '2',
+        fillOpacity: 0.5
+    }
+    var Reed = {            
+        fillColor: 'green',
+        weight: 1,
+        opacity: 1,
+        color: 'black',
+        dashArray: '2',
+        fillOpacity: 0.5
+    }
+
+    L.DrawToolbar.include({
+        getModeHandlers: function (map) {
+            return [
+                {
+                    enabled: true,
+                    handler: new L.Draw.Polygon(map, {shapeOptions: Miscanthus}),
+                    title: 'Place restaurant marker',
+                },
+                {
+                    enabled: true,
+                    handler: new L.Draw.Polygon(map, {shapeOptions: Maize}),
+                    title: 'Place gas station marker'
+                },
+                {
+                    enabled: true,
+                    handler: new L.Draw.Polygon(map, {shapeOptions: Cattail}),
+                    title: 'Place hospital marker'
+                },
+                {
+                    enabled: true,
+                    handler: new L.Draw.Polygon(map, {shapeOptions: Reed}),
+                    title: 'Place hospital marker'
+                }
+            ];
+        }
+    });
+
+    const _onCreate = e => {
+        setDraggable(false);
+        const {layerType, layer} = e;
+        const{_leaflet_id} = layer;
+        var color = layer.options.fillColor;
+        var area = L.GeometryUtil.geodesicArea(layer.getLatLngs()[0]);
+        if(layerType === "polygon"){
+            if (color === "white") {
+                //var popup = layer.bindPopup(popupContent);
+                let energy = Math.round(246.15 * area/10000);
+                let carbon = Math.round(14.85 * area/10000);
+                setTotalMisEnergy(totalMisEnergy => totalMisEnergy+ energy);
+                setTotalMisArea (totalMisArea => totalMisArea + area);
+                setTotalMisCarbon (totalMisCarbon => totalMisCarbon + carbon);
+                settotalEnergy (totalEnergy => totalEnergy + energy);
+                settotalCarbon (totalCarbon => totalCarbon + carbon);
+                setMapLayers(layers => [...layers, {id: _leaflet_id, latLngs: layer.getLatLngs()[0], area: area, type: "Miscanthus"}]);
+                console.log("white");
+            } else if (color === "red") {
+                //var popup = layer.bindPopup(popupContent);
+                let energy = Math.round(520.38 * area/10000);
+                let carbon = Math.round(29.453 * area/10000);
+                setTotalMaizArea (totalMaizArea => totalMaizArea + area);
+                setTotalMaizEnergy (totalMaizEnergy => totalMaizEnergy + energy);
+                setTotalMaizCarbon (totalMaizCarbon => totalMaizCarbon + carbon);
+                settotalEnergy (totalEnergy => totalEnergy + energy);
+                settotalCarbon (totalCarbon => totalCarbon + carbon);
+                setMapLayers(layers => [...layers, {id: _leaflet_id, latLngs: layer.getLatLngs()[0], area: area, type: "Maize"}]);
+                console.log("red");
+            } else if (color === "blue") {
+                //var popup = layer.bindPopup(popupContent);
+                let energy = Math.round(128.32 * area/10000);
+                let carbon = Math.round(7.263 * area/10000);
+                setTotalCatArea (totalCatArea => totalCatArea + area);
+                setTotalCatenergy (totalCatEnergy => totalCatEnergy + energy);
+                setTotalCatCarbon (totalCatCarbon => totalCatCarbon + carbon);
+                settotalEnergy (totalEnergy => totalEnergy + energy);
+                settotalCarbon (totalCarbon => totalCarbon + carbon);
+                setMapLayers(layers => [...layers, {id: _leaflet_id, latLngs: layer.getLatLngs()[0], area: area, type: "Cattail"}]);
+                console.log("blue");
+            } else {
+                console.log("else");
+                //var popup = layer.bindPopup(popupContent);
+                let energy = Math.round(262.46 * area/10000);
+                let carbon = Math.round(14.855 * area/10000);
+                setTotalReedArea (totalReedArea => totalReedArea + area);
+                setTotalReedenergy (totalReedenergy => totalReedenergy + energy);
+                setTotalReedCarbon(totalReedCarbon => totalReedCarbon + carbon);
+                settotalEnergy (totalEnergy => totalEnergy + energy);
+                settotalCarbon (totalCarbon => totalCarbon + carbon);
+                setMapLayers(layers => [...layers, {id: _leaflet_id, latLngs: layer.getLatLngs()[0], area: area, type: "Reed"}]);
+            }
+        };
+        settotalArea(totalArea => totalArea + area);
+
+    };
+
+    
+    const _onEdited = e => {
+        const {layers: {_layers}} = e;
+        Object.values(_layers).map(({_leaflet_id, editing}) => {
+            setMapLayers( layers => layers.map( l => l.id === _leaflet_id ? {...l, latlngs: { ...editing.latlngs[0]}}: l));
+        });
+    };
+
+    const _onDeleted = (e) => {
+        const { layers: {_layers}} = e;
+        Object.values(_layers).map(({_leaflet_id}) => {
+            setMapLayers( layers => 
+                layers.filter( l => l.id !== _leaflet_id));
+        });
+    };
+
+    //total area
+    //=========================================================
+    const infrastructures = mapLayers.length;
+
+
+    let myFilter =[
+        'grayscale: 40%',
+    ];
+
+    //Marker Setup
+    //==============================================================================================
+        delete L.Icon.Default.prototype._getIconUrl;
+
+        L.Icon.Default.mergeOptions({
+            iconRetinaUrl: 'https://cdn4.iconfinder.com/data/icons/ecology-environmentalism-line/56/Untitled-1-34-512.png',
+            iconUrl: 'https://cdn4.iconfinder.com/data/icons/ecology-environmentalism-line/56/Untitled-1-34-512.png',
+            shadowUrl: '',
+        });
+    
+
+    //list of Wind Turbines control
+    //==============================================================================================
+    // function TurbineList(props) {
+    //     const turbines = props.numbers;
+    //     const listTurbines = turbines.map((number) =>
+    //         <li className="infralist"key={number.id}>
+    //             <p>Infrastrucutre Type: {number.type}</p>
+    //             <p>Infrastrucutre ID: {number.id}</p>
+    //             <p>Infrastructure Location/area: {number.latLngs.lat} {number.latLngs.lng} {number.area}</p>
+    //             {/* <p>{number.popup}</p> */}
+    //         </li>
+    //     );
+    //     return (
+    //         <ul className="turbine"> {listTurbines}</ul>
+    //     );
+    // }
+    
+    //=======================Form =====================//
+        const getInputValue = (event)=> {
+            const userValue = event.target.value;
+            setBufferValue(userValue);
+            console.log(bufferValue);
+        }
+
+    //========================== Update Buffer =================//
+    function updateBuffer() {
+        var bufferZone = L.featureGroup().addTo(map);      
+        var buffered = buffer(multipleGeojson, bufferValue, {units: 'meters'});
+        var buff = L.geoJson(buffered);
+        buff.addTo(bufferZone);   
+        var checked = bufferZone.getLayers().length;
+        console.log(checked);     
+    }
+    //=========================== Graph =========================//
+    
+
+        
+    
     return(
-        <Layout>
-            <section className="centerpage">
-                <div className="center-container">
-                <div className="long"></div>
-                    <h1 className="EVselectormenu">Regional Energy Transition Tool</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                    <div className="short"></div>
-                    <Link to='/Areas' className="no-decoration"><h4 className="learnmore">Start </h4></Link>
-                </div>
-            </section>
-        </Layout>
+        <section>
+        <div className="mapcontainerof">
+        <MapContainer style={{ height: "90vh", width: "100vww" }} center={[52.473351, 6.667982]} zoom={13.5} scrollWheelZoom={true} crs={L.CRS.EPSG3857} whenCreated ={setMap} draggable= {false}>
+        <LayersControl position="topright">
+            <LayersControl.BaseLayer checked name="OpenStreetMap.Mapnik">
+            <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            filter= "myFilter"
+            />
+        </LayersControl.BaseLayer>
+            <FeatureGroup>
+                <EditControl 
+                    position="topleft" 
+                    onCreated={_onCreate} 
+                    onEdited={_onEdited} 
+                    onDeleted={_onDeleted} 
+                    draw={{rectangle: false, polyline:false, circle: false, circlemarker: false, marker: false}}/>
+            </FeatureGroup>
+            <LayerGroup>
+                <Natuura2000 />
+                <LayersControl.Overlay name="Natuura 2000 Buffer">
+                <FeatureGroup name="buffered">
+                    <GeoJSON 
+                        data={buffered}
+                        style={{ color: "gold", opacity: 1, fillOpacity: 0.4 }}
+                    />
+                </FeatureGroup>
+                </LayersControl.Overlay>
+            </LayerGroup>
+        </LayersControl>
+        </MapContainer>
+        </div>
+        {/* <div className="detailsofmap">
+            <div className="icons">
+                <img src={miscanthus} className="logosolarfarm"></img>
+            </div>
+            <div className="icons">
+                <img src={maze} className="logosolarfarm"></img>
+            </div>
+            <div className="icons">
+                <img src={cattail} className="logosolarfarm"></img>
+            </div>
+            <div className="icons">
+                <img src={reed} className="logosolarfarm"></img>
+            </div>
+            <div className="icons">
+                <p><span>Total Energy Produced:</span> {totalEnergy}</p>
+            </div>
+            <div className="icons">
+                <p><span>Total Co2 Emission Saving:</span> {totalCarbon}</p>
+            </div>
+        </div>
+        <div className="details">
+            <div>
+            </div>
+        </div> */}
+        <Bargraph totalMisArea={totalMisArea} totalMaizArea={totalMaizArea} totalCatArea={totalCatArea} totalReedArea={totalReedArea}  />
+        <StackedBargraph totalMisEnergy={totalMisEnergy} totalMaizEnergy={totalMaizEnergy} totalCatEnergy={totalCatEnergy} totalReedenergy={totalReedenergy} totalMisCarbon={totalMisCarbon} totalMaizCarbon={totalMaizCarbon} totalCatCarbon={totalCatCarbon} totalReedCarbon={totalReedCarbon}  />
+        <div className="bufferset">
+            <input type="text" onChange={getInputValue}/>
+            <button onClick ={updateBuffer}>Update the Buffer</button>
+        </div>
+        <pre className="text-left">{JSON.stringify(mapLayers, 0, 2)}</pre>
+        </section>
     );
 }
 
-export default Main;
+export default TempLocation;
+
+
+
+
