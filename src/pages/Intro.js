32bb86d3af3e6afbecbaf36d9c7a7a6d6ae0b008
Moozhan <moozhansh@gmail.com>
2022-09-30 16:02:21 +0100

    dependencies between react autocomplete

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -8,23 +8,23 @@ function Intro() {
     console.log("page loaded");
 
     return (
-        <section>
+        <section className="main-section-intro">
             <h1>Sustainable Agriculture Decision Support System</h1>
             <div className="container-paragraph">
                 <p>Does implementing energy transition mean abolishing argicultural acitivies? is horticulture a viable solution to overcome the impacts energy transition plans have on argicultural activities? SADSS was designed to help exploring such questions. As a decision support system, SADSS allow you to explore the horticulture implication in your area.  </p>
             </div>
-            <div className="grid">
-                <div className="grid-item">
-                    <img src={tutorial1} alt="Logo" />
+            <div className="intro-page-grid">
+                <div className="intro-page-grid-item">
+                    <img className="image-intro-page" src={tutorial1} alt="Logo" />
                 </div>
-                <div className="grid-item">
-                    <img src={tutorial1} alt="Logo" />
+                <div className="intro-page-grid-item">
+                    <img className="image-intro-page" src={tutorial1} alt="Logo" />
                 </div>
-                <div className="grid-item">
-                    <img src={tutorial1} alt="Logo" />
+                <div className="intro-page-grid-item">
+                    <img className="image-intro-page" src={tutorial1} alt="Logo" />
                 </div>
-                <div className="grid-item">
-                    <img src={tutorial1} alt="Logo" />
+                <div className="intro-page-grid-item">
+                    <img className="image-intro-page" src={tutorial1} alt="Logo" />
                 </div>
             </div>
             <button><Link to='/dss'>Click me</Link></button>
